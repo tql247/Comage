041052d8ffd34a7cf160cfadfac3454584f3c708
linhYedda <linh.yedda@yedda.ai>
2021-09-08 15:34:04 +0700

    add mode config

--- config.tsx
+++ config.tsx
@@ -1,8 +1,12 @@
+const mode = "production";
+const HOST = mode==="production"?"14.241.231.87":"localhost";
+const PORT = mode==="production"?"5001":"5000";
+
 export const APIConfig = {
     'api': {
-        'get_manga_info': 'http://localhost:5000/manga/get/info?mangaProviderID=',
-        'get_manga_chapters': 'http://localhost:5000/chapter/get?mangaId=',
-        'get_chapter_latest': 'http://localhost:5000/chapter/get_latest',
-        'get_chapter_details': 'http://localhost:5000/chapter/get_chapter_detail?chapterId=',
+        'get_manga_info': `http://${HOST}:${PORT}/manga/get/info?mangaProviderID=`,
+        'get_manga_chapters': `http://${HOST}:${PORT}/chapter/get?mangaId=`,
+        'get_chapter_latest': `http://${HOST}:${PORT}/chapter/get_latest`,
+        'get_chapter_details': `http://${HOST}:${PORT}/chapter/get_chapter_detail?chapterId=`,
     }
 }
