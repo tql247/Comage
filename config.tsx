23c047e28cfd07bf6efe0fe97d795605081399a0
linhYedda <linh.yedda@yedda.ai>
2021-09-11 18:40:46 +0700

    fix get list manga

--- config.tsx
+++ config.tsx
@@ -5,7 +5,7 @@ const PORT = mode==="production"?"5001":"5000";
 export const APIConfig = {
     'api': {
         'get_manga_info': `http://${HOST}:${PORT}/manga/get/info?mangaProviderID=`,
-        'get_manga_chapters': `http://${HOST}:${PORT}/chapter/get?mangaId=`,
+        'get_manga_chapters': `http://${HOST}:${PORT}/chapter/get?mangaProviderId=`,
         'get_chapter_latest': `http://${HOST}:${PORT}/chapter/get_latest`,
         'get_chapter_details': `http://${HOST}:${PORT}/chapter/get_chapter_detail?chapterId=`,
         'get_next_chapter': `http://${HOST}:${PORT}/chapter/get_next?chapterId={chapterId}&mangaProviderId={mangaProviderId}`,
